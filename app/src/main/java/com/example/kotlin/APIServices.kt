ed491cfdc740210d05d6d6bc35d3c514553093ac
bunrealcua <voduynhan.123@gmail.com>
2023-03-25 16:05:50 +0700

    build: integrate to admin bus create activity

--- app/src/main/java/com/example/kotlin/APIServices.kt
+++ app/src/main/java/com/example/kotlin/APIServices.kt
@@ -3,8 +3,9 @@ package com.example.kotlin
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
+import retrofit2.http.*
 
+const val BASE_URL = "http://192.168.1.6:3000/v1/"
 data class BusStation (
     val id: String,
     val name: String,
@@ -30,7 +31,7 @@ data class Bus(
     val num_of_seats: Int,
     val price: Int,
     val bus_operators: BusOperator,
-    val left_seats: Int
+//    val left_seats: Int ADMIN DOESN'T NEED THIS
 )
 
 data class BusResponse(
@@ -38,23 +39,45 @@ data class BusResponse(
     val data: List<Bus>
 )
 
-interface BusService {
-    @GET("/bus/search")
-    fun searchBusses(): Call<BusResponse>;
-}
-
 data class BusStationResponse(
     val data: List<BusStation>
 )
 
+// Structure of body for api request
+data class AdminBusCreateBody (
+    val bo_id: String,
+    val start_point: String,
+    val end_point : String,
+    val type: Int,
+    val start_time: String,
+    val end_time : String,
+    val image_url: String,
+    val policy: String,
+    val num_of_seats: Int,
+    val price: Int
+)
+
+
+interface BusService {
+    @GET("/bus/search")
+    fun searchBusses(): Call<BusResponse>;
+
+    // Admin create bus
+
+    @POST("admin/bus/create")
+    fun adminCreateBus(
+        @Header("Authorization") token: String,
+        @Body bus: AdminBusCreateBody): Call<Bus>
+}
+
 interface BusStationService {
-    @GET("/v1/bus-station/list")
+    @GET("/bus-station/list")
     fun getBusStations(): Call<BusStationResponse>;
 }
 
 class APIServiceImpl {
     val api = Retrofit.Builder()
-        .baseUrl("http://10.123.1.217:3000/")
+        .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
     fun searchBusses(): BusService {
@@ -63,5 +86,10 @@ class APIServiceImpl {
 
     fun getAllBusStations(): BusStationService {
         return api.create(BusStationService::class.java)
+    }
+
+    // Admin create bus
+    fun adminCreateBus(): BusService {
+        return  api.create(BusService::class.java)
     }
 }
--- app/src/main/java/com/example/kotlin/AdminBusCreateActivity.kt
+++ app/src/main/java/com/example/kotlin/AdminBusCreateActivity.kt
@@ -1,15 +1,72 @@
 package com.example.kotlin
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import retrofit2.awaitResponse
+
+
 
 class AdminBusCreateActivity: AppCompatActivity() {
+    lateinit var addBtn: Button
+
+    // THIS WILL BE REPLACED WHENEVER HOANG LOGIN BY FACEBOOK
+    // TOKEN WILL GET FROM THERE
+    val token = "Bearer  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3ZmU0YTNlZS0zMjRiLTQ0NWQtODYzYy0wN2ZjNzAyYmQ4NDQiLCJpYXQiOjE2Nzk3MzM5MTQsImV4cCI6MTY3OTczNTcxNCwidHlwZSI6ImFjY2VzcyJ9.uQsh3SoCWeCoYe3l5yMUfMhiSbQDGtEjsSZrM2RPTuA"
+
+    // Sample data
+    lateinit var sampleData : AdminBusCreateBody
+
 
     // TODO Fragment not activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_bus_create)
+
+        // Create sample data
+        sampleData = AdminBusCreateBody(
+            "fbb09364-3e67-46e2-bf0f-95670d827544",
+            "402bdf26-013e-4438-9813-b4d8c326e60d",
+            "402bdf26-013e-4438-9813-b4d8c326e60d",
+            0,
+            "01/03/2022",
+            "01/05/2022",
+            "https://loremflickr.com/300/200",
+            "Hello",
+            23,
+            53000
+        )
+
+
+
+
         // TODO Integrate
 
-    }
+        addBtn = findViewById(R.id.addBtnAdminBusCreateBtn)
+        val retrofit = APIServiceImpl()
+
+        addBtn.setOnClickListener {
+            val coroutineExceptionHandler = CoroutineExceptionHandler{_, throwable ->
+                throwable.printStackTrace()
+            }
+            // THIS LOOK LIKE I CAN CALL API SUCCESSFULLY BUT REPONSE IS NOT HANDLE CORRECTLY
+            GlobalScope.launch (Dispatchers.IO + coroutineExceptionHandler) {
+                val response = retrofit.adminCreateBus().adminCreateBus(token,sampleData).awaitResponse()
+                Log.d("Response", "vui 1" + response.message())
+                // debug response
+                Log.d("Response", response.toString())
+                if(response.isSuccessful){
+                    Log.d("Response", "vui 2")
+                    val data = response.body()!!
+                    Log.d("Response", data.toString())
+                }
+            }
+        }
+        }
+
 }
