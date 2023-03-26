c54ad817951c9710511f2e2428d6b9a653b468ad
simplord542 <81230977+simplord542@users.noreply.github.com>
2023-03-26 14:25:36 +0700

    fire base login fb

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -1,11 +1,83 @@
 package com.example.kotlin
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.facebook.*
+import com.facebook.login.LoginResult
+
+import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var callbackManager: CallbackManager
+    private lateinit var auth: FirebaseAuth
+    private lateinit var user: FirebaseUser
+    private lateinit var db: FirebaseDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FacebookSdk.sdkInitialize(applicationContext);
+        callbackManager = CallbackManager.Factory.create()
+        db = FirebaseDatabase.getInstance("https://kotlin-96709-default-rtdb.firebaseio.com/")
+        auth = Firebase.auth
+
+        var buttonFacebookLogin = findViewById<LoginButton>(R.id.login_button)
+        buttonFacebookLogin.setReadPermissions("email", "public_profile")
+        buttonFacebookLogin.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                //Log.d(TAG, "facebook:onSuccess:$loginResult")
+                handleFacebookAccessToken(loginResult.accessToken)
+            }
+
+            override fun onCancel() {
+                //Log.d(TAG, "facebook:onCancel")
+            }
+
+            override fun onError(error: FacebookException) {
+                //Log.d(TAG, "facebook:onError", error)
+            }
+        })
+
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+    }
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        //Log.d(TAG, "handleFacebookAccessToken:$token")
+
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    //Log.d(TAG, "signInWithCredential:success")
+                    user = auth.currentUser!!
+                    db.reference.child("users").child(user.uid).apply{setValue(user.displayName!!)
+                        child("fb_id").setValue(token.userId)
+                    }
+                    //assign tk vào backend ở đây,
+//                    var intent = Intent(this, MainActivity2::class.java)
+//                    startActivity(intent)
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    //Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Toast.makeText(this, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+
 }
