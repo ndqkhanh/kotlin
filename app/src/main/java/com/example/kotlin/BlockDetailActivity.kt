b997a38121da1acf8b3c048383f184852bacc21d
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-03-27 16:49:41 +0700

    refactor: refactor name of blog xml file

--- app/src/main/java/com/example/kotlin/BlockDetailActivity.kt
+++ app/src/main/java/com/example/kotlin/BlockDetailActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class BlockDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_block_detail)
+        setContentView(R.layout.activity_blog_detail)
     }
 }
