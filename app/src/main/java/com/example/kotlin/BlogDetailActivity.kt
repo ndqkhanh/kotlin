b8326c5ca98809d56f620f288767ff459f8777c1
Nguyễn Đinh Quang Khánh <20127530@student.hcmus.edu.vn>
2023-03-29 16:41:38 +0700

    refator: change name

--- app/src/main/java/com/example/kotlin/BlogDetailActivity.kt
+++ app/src/main/java/com/example/kotlin/BlogDetailActivity.kt
@@ -3,7 +3,7 @@ package com.example.kotlin
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class BlockDetailActivity : AppCompatActivity() {
+class BlogDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blog_detail)
