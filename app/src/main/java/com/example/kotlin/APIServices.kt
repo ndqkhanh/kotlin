5245c5d83824dad03f620a88b4c4e8d8b8228cf7
jacknguyen <nguyenphucbao098@gmail.com>
2023-04-24 15:19:46 +0700

    update template

--- app/src/main/java/com/example/kotlin/APIServices.kt
+++ app/src/main/java/com/example/kotlin/APIServices.kt
@@ -294,7 +294,7 @@ interface BusOperatorService {
 
 
 class APIServiceImpl {
-    private val BASE_URL = "http://192.168.1.10:3000/v1/"
+    private val BASE_URL = "http://10.124.7.76:3000/v1/"
 
     private val api: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
