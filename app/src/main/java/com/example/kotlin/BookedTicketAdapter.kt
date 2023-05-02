67f436be95595db25f8e379cba2770e9bd407a49
simplord542 <81230977+simplord542@users.noreply.github.com>
2023-05-02 12:28:40 +0700

    item_ve

--- app/src/main/java/com/example/kotlin/BookedTicketAdapter.kt
+++ app/src/main/java/com/example/kotlin/BookedTicketAdapter.kt
@@ -4,16 +4,14 @@ import android.app.Activity
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.TextView
 import com.example.kotlin.jsonConvert.History
 
 class BookedTicketAdapter
     (private val context: Activity, private var items: List<History>)
-    : ArrayAdapter<History>(context, R.layout.item_ticket, items){
+    : ArrayAdapter<History>(context, R.layout.item_ve, items){
         override fun getView(position: Int, convertView: View?, parent: ViewGroup): View{
             val inflater = context.layoutInflater
-            val rowView: View = inflater.inflate(R.layout.item_ticket, null, true)
+            val rowView: View = inflater.inflate(R.layout.item_ve, null, true)
 
 
             return rowView
