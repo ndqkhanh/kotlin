c08408bdb47e7c4c9b641dd346d97736b79cf9a4
bunrealcua <voduynhan.123@gmail.com>
2023-05-10 17:10:50 +0700

    add edit text for policy bus

--- app/src/main/java/com/example/kotlin/Admin/Screen/Bus/AdminBusCreateActivity.kt
+++ app/src/main/java/com/example/kotlin/Admin/Screen/Bus/AdminBusCreateActivity.kt
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog.OnDateSetListener
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.text.Spanned
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,7 @@ import android.view.Window
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin.*
@@ -47,7 +49,7 @@ class AdminBusCreateActivity: AppCompatActivity() {
     private var photoChosen = false
     private var photoUri: Uri? = null
     private var fileUpload = UploadFile()
-
+    private lateinit var edtContent: com.google.android.material.textview.MaterialTextView
 
     private var busType: MutableList<String> = mutableListOf("Ghế ngồi", "Giường nằm", "Giường nằm đôi")
 
@@ -71,7 +73,6 @@ class AdminBusCreateActivity: AppCompatActivity() {
     val token = "BEARER " + UserInformation.TOKEN
     val retrofit = APIServiceImpl()
 
-    // TODO Fragment not activity
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_bus_create)
@@ -226,6 +227,15 @@ class AdminBusCreateActivity: AppCompatActivity() {
             selectImage()
         }
 
+
+        edtContent = findViewById(R.id.edtPolicy)
+        // intent to EditText Detail Activity when click on edtContent
+        edtContent.setOnClickListener {
+            val intent = Intent(this, EditTextDetailActivity::class.java)
+            intent.putExtra("content", HtmlCompat.toHtml(edtContent.text as Spanned, HtmlCompat.TO_HTML_PARAGRAPH_LINES_CONSECUTIVE))
+            startActivityForResult(intent, 567)
+        }
+
         // add new bus
         addBtn.setOnClickListener {
             // get start time, end time
@@ -241,7 +251,7 @@ class AdminBusCreateActivity: AppCompatActivity() {
             Log.d("start_time ", start_time)
 
 
-            policy = "Hello"
+            policy = edtContent.text.toString()
 
             // get number of seats
             if (numOfSeatET.text.toString() != "")
@@ -257,7 +267,7 @@ class AdminBusCreateActivity: AppCompatActivity() {
             }
 
 
-            if(bo_id.isEmpty() || start_point.isEmpty() || end_point.isEmpty() || start_time.isEmpty() || end_time.isEmpty() || num_of_seats == 0 || price == 0){
+            if(bo_id.isEmpty() || start_point.isEmpty() || end_point.isEmpty() || start_time.isEmpty() || end_time.isEmpty() || num_of_seats == 0 || price == 0 || policy.isEmpty()){
                 Toast.makeText(this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
@@ -384,6 +394,17 @@ class AdminBusCreateActivity: AppCompatActivity() {
                 image.setImageURI(imageUri)
                 photoUri = imageUri
                 photoChosen = true
+            }
+        }else if(requestCode == 567) {
+            if (resultCode == Activity.RESULT_OK) {
+                // get content from EditTextDetailActivity
+                val content = data?.getStringExtra("content")
+                edtContent.setText(content?.let {
+                    HtmlCompat.fromHtml(
+                        it,
+                        HtmlCompat.FROM_HTML_MODE_LEGACY
+                    )
+                })
             }
         }
     }
